6e162db1e8df00ade14898027029522239eb1382
vrotondo <vrfr91@gmail.com>
2025-05-27 16:20:38 -0400

    Edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'dandd-homebrew-creator.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ]
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  }
 })
